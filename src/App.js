8f3bb48e6821e834b2064ca003161c6ab3c00596
kahee <yuygh131@gmail.com>
2024-08-27 21:33:14 +0900

    add: 6.0 state 문제 확인

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import Button  from './Button';
-import styles from "./App.module.css";
+import Button from './Button';
+import styles from './App.module.css';
+import { useState } from 'react';
 
 function App() {
+  // state 가 변화할때마다 계속 리랜더링 되는데 이때마다 불필요하게 재호출됨
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"}/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}> click me </button>
     </div>
   );
 }
