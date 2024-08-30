bdf975185778f841c7d9d6e3939f9c3137926302
kahee <yuygh131@gmail.com>
2024-08-30 17:10:26 +0900

    add: 7.0 to do list part one

--- src/App.js
+++ src/App.js
@@ -4,39 +4,31 @@ import { useState, useEffect } from 'react';
 import { keyboard } from '@testing-library/user-event/dist/keyboard';
 
 function App() {
-  // state 가 변화할때마다 계속 리랜더링 되는데 이때마다 불필요하게 재호출됨
-  const [counter, setValue] = useState(0);
-  const [kyeword, setKeyword] = useState('');
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log('i run all, the time');
-  const iRunOnlyOnce = () => {
-    console.log('i run only once');
-  };
-  // 한번만 호출하고 다시 호출하지 않음 only once
-  useEffect(iRunOnlyOnce, []);
-  // keyword 가 변할때만 리랜더링 하고 싶음 -> 특정부분만 변화할때, 원하는코드들 실행되도록
-  useEffect(() => {
-    if (kyeword !== '' && kyeword.length > 5) {
-      console.log({ kyeword });
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === '') {
+      return;
     }
-  }, [kyeword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes");
-  }, [counter]);
-  useEffect(() => {
-    console.log('I run when keyword & counter change');
-  }, [counter, kyeword]);
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo('');
+  };
+  console.log(toDos);
   return (
     <div>
-      <input
-        value={kyeword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search hear..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}> click me </button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do"
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
